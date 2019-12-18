3cc7c9b7ee999e9b085cc7bdc404ce3e67da3c04
Khani Launggu <kanilaunggu@gmail.com>
2019-12-18 10:30:56 +0700

    GO: Tambah BilanganReal.java

---
+++ Modul4/BilanganReal.java
@@ -0,0 +1,18 @@
+public class BilanganReal {
+    public static void main(String[] args) 	{
+          //Deklarasi float
+      float  tipeFloat1    = 0.13f;
+      float  tipeFloat2   = .25f;
+      //Deklarasi double
+      double  tipeDouble1 = 0.4e-42;
+      double  tipeDouble2 = 432.12D;
+      //Cetak
+  System.out.println("Bilangan Real - Desimal  ");
+  System.out.println("----------------------------------");
+      System.out.println("Tipe Float tipeFloat1          = "+ tipeFloat1);
+      System.out.println("Tipe Float tipeFloat2          = "+ tipeFloat2);
+      System.out.println("Tipe Double tipeDouble1        = "+ tipeDouble1);
+      System.out.println("Tipe Double tipeDouble2        = "+ tipeDouble2);		
+    }
+  }
+  
