ac4ba8f112a6f51a5c1245684d7b44822d7b27c1
Khani Launggu <kanilaunggu@gmail.com>
2019-12-18 10:33:26 +0700

    GO: Tambah TipeChar.java

---
+++ Modul4/TipeChar.java
@@ -0,0 +1,17 @@
+public class TipeChar {
+    public static void main(String[] args) 	{
+      //Deklarasi char
+      char  char1   = 'a';
+      char  char2   = 0x0ff32; //adalah '?'
+      char  char3   = '\t'; //tab
+  
+      //Cetak
+      System.out.println("Tipe Data Char");
+      System.out.println("----------------------------------");		
+        System.out.println("Tipe Char char1          = "+ char1);
+        System.out.println("Tipe Char char2          = "+ char2);
+        System.out.println("Tipe Char char3          = a"+ char3+"b");
+          
+    }
+  }
+  
