fd75e5d59d7e9328f2b88ce6bee440159fedea6b
Khani Launggu <kanilaunggu@gmail.com>
2019-12-18 10:21:42 +0700

    GO: Tambah BilanganBulat.java

---
+++ Modul4/BilanganBulat.java
@@ -0,0 +1,17 @@
+public class BilanganBulat {
+    public static void main(String[] args) 	{
+          //Deklarasi
+      byte  tipeByte  = 50;
+      short tipeShort = 0x7e23;
+      int   tipeInt   = 21466666;
+      long  tipeLong  = 922000000;	
+      //Cetak
+      System.out.println("Bilangan Bulat  ");
+      System.out.println("----------------------------------");
+      System.out.println("Tipe Byte tipeByte  	= "+ tipeByte  );
+      System.out.println("Tipe Short tipeShort 	= "+ tipeShort  );
+      System.out.println("Tipe Int tipeInt        = "+ tipeInt  );
+      System.out.println("Tipe Long tipeLong   	= "+ tipeLong  );
+    }
+  }
+  
