769951789b45c4cfd49718a4fc309f282d6163db
khanilaunggu <kanilaunggu@Kanis-MacBook-Pro.local>
2019-12-30 22:20:18 +0700

    Semua file

---
+++ Modul6/Praktikum01/src/tipeData/tipeBoolean.java
@@ -0,0 +1,24 @@
+package tipeData;
+
+public class tipeBoolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Deklarasi
+		boolean booleanKondisi, booleanStatus;
+		int nilai1,nilai2;
+		//Isi variabel nilai1 dan nilai2
+		nilai1 = 7;
+		nilai2 = 10;
+
+		booleanKondisi = nilai1 > nilai2;
+		booleanStatus = true;
+		//Cetak
+		System.out.println("Tipe Data Boolean");
+		System.out.println("----------------------------------");		
+		System.out.println("Nilai BooleanKondisi 7 > 10  = "+ booleanKondisi);
+		System.out.println("Nilai booleanStatus          = "+ booleanStatus);
+
+	}
+
+}
---
+++ Modul6/Praktikum01/src/tipeData/tipeChar.java
@@ -0,0 +1,21 @@
+package tipeData;
+
+public class tipeChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  //Deklarasi char
+		  char  char1   = 'a';
+		  char  char2   = 0x0ff32; //adalah '?'
+		  char  char3   = '\t'; //tab
+
+		  //Cetak
+		  System.out.println("Tipe Data Char");
+		  System.out.println("----------------------------------");		
+		  System.out.println("Tipe Char char1          = "+ char1);
+		  System.out.println("Tipe Char char2          = "+ char2);
+		  System.out.println("Tipe Char char3          = a"+ char3+"b");
+
+	}
+
+}
